baeca5b0c5ec1db22ccb6bb248ae1be7e1a7fa40
git-ADG <adg8503@gmail.com>
2024-02-23 15:34:29 +0530

    loading overlay added

---
+++ lib/dialogs/loading_overlay/loading_overlay.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+Future<void> showLoadingOverlay(
+    {required BuildContext parentContext,
+    ValueNotifier<bool>? isVisible,
+    void Function()? onCompleted,
+    Future Function()? asyncTask}) async {
+  isVisible ??= ValueNotifier(true);
+  Navigator.push(
+    parentContext,
+    PageRouteBuilder(
+      opaque: false,
+      barrierColor: Colors.black12,
+      pageBuilder: (context, _, __) {
+        return Theme(
+          data: ThemeData.light(),
+          child: PopScope(
+            canPop: false,
+            onPopInvoked: (didPop) {
+              if (didPop) {
+                return;
+              }
+            },
+            child: ValueListenableBuilder<bool>(
+                valueListenable: isVisible!,
+                builder: (BuildContext context, visible, Widget? child) {
+                  if (!visible) {
+                    SchedulerBinding.instance
+                        .scheduleFrameCallback((_) => Navigator.pop(context));
+                  }
+                  return const Center(
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }),
+          ),
+        );
+      },
+    ),
+  ).then((value) {
+    if (onCompleted != null) {
+      onCompleted();
+    }
+  });
+  if (asyncTask != null) {
+    try {
+      await asyncTask();
+    } catch (e) {
+      rethrow;
+    } finally {
+      isVisible.value = false;
+    }
+  }
+}
