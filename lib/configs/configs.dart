cf4af1280b7b15858b71329bd8614ce0f61b66c5
git-ADG <adg8503@gmail.com>
2024-02-23 15:36:34 +0530

    export files edited

--- lib/configs/configs.dart
+++ lib/configs/configs.dart
@@ -1,3 +1,6 @@
 //export
 export 'configurations/extensions/constants.dart';
 export 'configurations/theme/utils/palette.dart';
+export 'configurations/extensions/custom_spacers.dart';
+export 'configurations/extensions/gap.dart';
+export 'configurations/extensions/screen_utils.dart';
---
+++ lib/dialogs/exit_message.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+Future<bool> showExitWarning(BuildContext context) async {
+  bool choice = false;
+  if (Navigator.canPop(context)) {
+    Navigator.pop(context);
+  } else {
+    await showDialog<bool>(
+        context: context,
+        useRootNavigator: false,
+        builder: (context) => AlertDialog(
+          title: const Text("Do you want to exit the app?"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  choice = false;
+                  Navigator.pop(context);
+                },
+                child: const Text("No")),
+            TextButton(
+                onPressed: () {
+                  choice = true;
+                  Navigator.pop(context);
+                },
+                child: const Text("Yes"))
+          ],
+          alignment: Alignment.center,
+          actionsAlignment: MainAxisAlignment.spaceEvenly,
+        ));
+  }
+  return choice;
+}
