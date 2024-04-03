9f16c2756633b564243f1af982a11f5e21d49ca9
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-04-04 00:22:19 +0530

    added the dark theme

---
+++ lib/configs/configurations/theme/themes/dark_theme.dart
@@ -0,0 +1,10 @@
+import 'package:ecell_app/configs/configs.dart';
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: const ColorScheme.dark(
+    primary: dark,
+    secondary: ecellBlue,
+  ),
+);
