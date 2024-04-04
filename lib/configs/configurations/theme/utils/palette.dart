e476c1f63a29fdd118b94e73dd456613bdbae981
git-ADG <adg8503@gmail.com>
2024-04-04 22:59:31 +0530

    custom background created

--- lib/configs/configurations/theme/utils/palette.dart
+++ lib/configs/configurations/theme/utils/palette.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 //color
 const Color ecellBlue = Color(0xFF0E3669);
+const Color lightBlue = Color(0xFF0051B1);
 const Color dark = Colors.black;
---
+++ lib/utils/widgets/custom_background/custom_background.dart
@@ -0,0 +1,65 @@
+import 'package:ecell_app/configs/configs.dart';
+import 'package:flutter/material.dart';
+
+class CustomBackground extends StatelessWidget {
+  const CustomBackground({super.key, required this.bodyWidget});
+  final Widget bodyWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    //abstract circles portion to be reused in other pages
+    return Stack(
+      children: [
+        Positioned(
+          top: -40,
+          left: 197,
+          child: Container(
+            width: 116,
+            height: 116,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: lightBlue,
+            ),
+          ),
+        ),
+        Positioned(
+          top: -40,
+          left: 242,
+          child: Container(
+            width: 227,
+            height: 227,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: ecellBlue,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 672,
+          left: -94,
+          child: Container(
+            width: 227,
+            height: 227,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: lightBlue,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 710,
+          left: 60,
+          child: Container(
+            width: 116,
+            height: 116,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: ecellBlue,
+            ),
+          ),
+        ),
+        bodyWidget,
+      ],
+    );
+  }
+}
