119a88edc2f42cb482d5ad05d47ce1194c763bd3
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-04-06 00:13:44 +0530

    added moto icons

--- lib/configs/configurations/assets/asset.dart
+++ lib/configs/configurations/assets/asset.dart
@@ -1,4 +1,8 @@
 const String ecellLogo = 'assets/images/ecell-logo-bw2_sayvqp.png';
-const String wwp1 = 'assets/images/Property_1=Default.png';
-const String wwp2 = 'assets/images/Property_1=Variant2.png';
-const String wwp3 = 'assets/images/Property_1=Variant3.png';
+const String wwp1 = 'assets/images/provide.png';
+const String wwp2 = 'assets/images/provide1.png';
+const String wwp3 = 'assets/images/provide2.png';
+const String moto1 = 'assets/images/fluent-emoji-flat_light-bulb.png';
+const String moto2 = 'assets/images/hand-01.png';
+const String moto3 = 'assets/images/moto-3_thwnrc-01.png';
+const String moto4 = 'assets/images/moto-4_krbq3h-01.png';
