be5c1ed80338d11cee2fbc1031d29255458c51c0
Rishav Kumar Sinha <sinharishavkumar@gmail.com>
2024-09-03 22:25:34 +0530

    UI for Profile Page Added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ecell_app/configs/configs.dart';
 import 'package:ecell_app/pages/collabs_page/collabs_page.dart';
 import 'package:ecell_app/pages/home_page/utils/icon_provider.dart';
 import 'package:ecell_app/pages/login_page/login_page.dart';
+import 'package:ecell_app/pages/profile_page/profile_page.dart';
 import 'package:ecell_app/pages/signup_page/signup_page.dart';
 import 'package:ecell_app/pages/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -30,6 +31,7 @@ class ECellApp extends StatelessWidget {
           HomePage.routeName: (context) => const HomePage(),
           SplashScreen.routeName: (context) => const SplashScreen(),
           CollabPage.routeName: (context) => const CollabPage(),
+          ProfilePage.routeName: (context) => const ProfilePage(),
         },
         initialRoute: SplashScreen.routeName,
         // initialRoute: CollabPage.routeName,
