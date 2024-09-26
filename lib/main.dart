e82d65c045efa6c2f35fea5c01ca6d130e9f949f
mannate14 <mannatejain14@gmail.com>
2024-09-26 16:22:04 +0100

    events page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:ecell_app/configs/configs.dart';
+import 'package:ecell_app/pages/Events-page/Event_registration.dart';
+import 'package:ecell_app/pages/Events-page/Events-page.dart';
 import 'package:ecell_app/pages/collabs_page/collabs_page.dart';
 import 'package:ecell_app/pages/home_page/utils/icon_provider.dart';
-import 'package:ecell_app/pages/login_page/login_page.dart';
-import 'package:ecell_app/pages/signup_page/signup_page.dart';
 import 'package:ecell_app/pages/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,15 +23,14 @@ class ECellApp extends StatelessWidget {
         title: "E-Cell App",
         theme: darkTheme,
         debugShowCheckedModeBanner: false,
-        home: const HomePage(),
+        home: const Eventspage(),
         routes: {
-          LoginPage.routeName: (context) => const LoginPage(),
-          SignUpPage.routeName: (context) => const SignUpPage(),
+          Eventspage.routeName: (context) => const Eventspage(),
           HomePage.routeName: (context) => const HomePage(),
           SplashScreen.routeName: (context) => const SplashScreen(),
           CollabPage.routeName: (context) => const CollabPage(),
+          EventRegistrationpage.routeName: (context) => const EventRegistrationpage(),
         },
-        initialRoute: SplashScreen.routeName,
         // initialRoute: CollabPage.routeName,
       ),
     );
