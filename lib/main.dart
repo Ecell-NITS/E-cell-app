da93cbfc9fdc39ed583795e6e54ceac691664e8f
Krishna <krishnamewara841@gmail.com>
2024-10-17 18:33:56 +0530

    Merge changes from upstream

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:ecell_app/configs/configs.dart';
-<<<<<<< HEAD
+
 import 'package:ecell_app/pages/collabs_page/collabs_page.dart';
 import 'package:ecell_app/pages/gallery_page/gallery_page.dart';
 import 'package:ecell_app/pages/home_page/utils/icon_provider.dart';
@@ -9,9 +9,9 @@ import 'package:ecell_app/pages/resources_page/resources_page.dart';
 import 'package:ecell_app/pages/signup_page/signup_page.dart';
 import 'package:ecell_app/pages/splash_screen/splash_screen.dart';
 import 'package:ecell_app/pages/teams_page/teams_page.dart';
-=======
+
 import 'package:ecell_app/pages/home/utils/icon_provider.dart';
->>>>>>> upstream/main
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/pages.dart';
