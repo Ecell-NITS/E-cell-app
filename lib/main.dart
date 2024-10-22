721a95475cb83217e15e93cac2f1b3630a23a79f
Krishna <krishnamewara841@gmail.com>
2024-10-23 00:10:47 +0530

    gallery page made and resources page modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
 import 'package:ecell_app/configs/configs.dart';
-
-import 'package:ecell_app/pages/collabs_page/collabs_page.dart';
 import 'package:ecell_app/pages/gallery_page/gallery_page.dart';
-import 'package:ecell_app/pages/home_page/utils/icon_provider.dart';
-import 'package:ecell_app/pages/login_page/login_page.dart';
-import 'package:ecell_app/pages/profile_page/profile_page.dart';
-import 'package:ecell_app/pages/resources_page/resources_page.dart';
-import 'package:ecell_app/pages/signup_page/signup_page.dart';
-import 'package:ecell_app/pages/splash_screen/splash_screen.dart';
-import 'package:ecell_app/pages/teams_page/teams_page.dart';
-
 import 'package:ecell_app/pages/home/utils/icon_provider.dart';
 
 import 'package:flutter/material.dart';
@@ -42,6 +32,7 @@ class ECellApp extends StatelessWidget {
           ProfilePage.routeName: (context) => const ProfilePage(),
           ResourcePage.routeName: (context) => const ResourcePage(),
           TeamsPage.routeName: (context) => const TeamsPage(),
+          GalleryPage.routeName: (context) => const GalleryPage(),
 
 
           LoginPage.routeName: (context) => const LoginPage(),
